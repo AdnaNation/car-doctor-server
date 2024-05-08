fd9f22d47332f09c6599c94dafdbfef6ec76adf6
AdnaNation <azizulislamadnan@gmail.com>
2024-05-08 14:45:46 +0600

    some operation added

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ async function run() {
     await client.connect();
 
     const serviceCollection = client.db('carDotor').collection('services');
+    const bookingCollection = client.db('carDotor').collection('bookings');
 
     app.get('/services', async (req, res) => {
         const cursor = serviceCollection.find();
@@ -48,6 +49,31 @@ async function run() {
         const result = await serviceCollection.findOne(query, options);
         res.send(result);
     })
+
+        // bookings 
+        app.get('/bookings', async (req, res) => {
+          console.log(req.query.email);
+          let query = {};
+          if (req.query?.email) {
+              query = { email: req.query.email }
+          }
+          const result = await bookingCollection.find(query).toArray();
+          res.send(result);
+      })
+
+    app.post('/bookings', async (req, res) => {
+      const booking = req.body;
+      console.log(booking);
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+  });
+
+  app.delete('/bookings/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) }
+    const result = await bookingCollection.deleteOne(query);
+    res.send(result);
+})
 
 
     // Send a ping to confirm a successful connection
